44a75f3b400f93ee157453bb6b940f858254e505
Devinder Khroad <dkhroad@gmail.com>
2018-12-04 04:34:46 -0800

    update truffle config

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -20,13 +20,17 @@ module.exports = {
   // to customize your Truffle configuration!
 
   networks: {
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + token_key);
+      },
+      network_id: '4',
+    },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + token_key);
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/" + token_key);
       },
       network_id: '3',
-      gas: 4500000,
-      gasPrice: 10000000000
     },
     local: {
       provider: function() {

cbbd53ef2b169da3aa40bc3f35d7d4af3dc64cb3
Devinder Khroad <dkhroad@gmail.com>
2018-12-03 21:17:02 -0800

    add testnet configuration

--- truffle.js
+++ truffle.js
@@ -11,8 +11,28 @@
  *     gasPrice: 10000000000,
  *   },
  */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNEMONIC;
+var token_key = process.env.TOKEN_KEY;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + token_key);
+      },
+      network_id: '3',
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
+    local: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/");
+      },
+      network_id: '*',
+    },
+  }
 };

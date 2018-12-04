629bdbdfeeb14b7160c0d8d30f16cfc5cb812753
Devinder Khroad <dkhroad@gmail.com>
2018-12-03 21:12:16 -0800

    StarNotary enhancements
    Add additonal metadata to star info
    Configure uniqueness with the stars

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -1,20 +1,52 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
-import  "./ERC721Token.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
-contract StarNotary is ERC721Token {
+contract StarNotary is ERC721 {
     struct Star {
         string name;
+        string ra;
+        string dec;
+        string mag;
+        string starStory;
     }
 
-    mapping(uint256 => Star) public tokenIdToStarInfo; 
+    mapping(uint256 => Star) public tokenIdToStarMapping; 
   
     mapping(uint256 => uint256) public starForSale; 
 
-    function createStar(string _name,uint256 _tokenId) public {
-        Star memory NewStar = Star(_name);
-        tokenIdToStarInfo[_tokenId] = NewStar;
-        ERC721Token.mint(_tokenId);
+    mapping(bytes32 => bool) public starExists;
+
+    function createStar(string _name, 
+                        string _ra,
+                        string _dec,
+                        string _mag,
+                        string _starStory,
+                        uint256 _tokenId) public {
+        // input parameter valiadation
+        require(bytes(_dec).length > 0 &&
+                bytes(_ra).length > 0  && 
+                bytes(_mag).length > 0 &&
+                _tokenId != 0,
+                "missing args");
+
+        // ensure token id doesn't already exist
+        require(bytes(tokenIdToStarMapping[_tokenId].dec).length == 0 && 
+                bytes(tokenIdToStarMapping[_tokenId].ra).length == 0,
+                "token id already exists");
+
+        // ensure star doesn't exist 
+        bytes32  hash = uniqueHash(_ra,_dec,_mag);
+
+        // intentionally not calling checkIfStarExist()
+        // because calling that function requires computing hash twice 
+        // which is not economical (would cause more gas)
+        require(starExists[hash] == false,"Star is already claimed");
+
+        Star memory NewStar = Star(_name,_ra,_dec,_mag,_starStory);
+        starExists[hash]  = true;
+        tokenIdToStarMapping[_tokenId] = NewStar;
+        _mint(msg.sender,_tokenId);
     }
 
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
@@ -25,13 +57,14 @@ contract StarNotary is ERC721Token {
     function buyStar(uint256 _tokenId) public payable {
         require(starForSale[_tokenId] > 0,"Star must be for sale");
         uint256 starCost  = starForSale[_tokenId];
-        require(msg.value >= starCost);
+        require(msg.value >= starCost,"insufficient funds");
         address starOwner = this.ownerOf(_tokenId);
 
-        clearPreviousStarState(_tokenId);
-        ERC721Token.transferFromHelper(starOwner,msg.sender,_tokenId);
+        _removeTokenFrom(starOwner, _tokenId);
+        _addTokenTo(msg.sender,_tokenId);
 
         starOwner.transfer(starCost);
+        
         // return overpaid amount 
         if (msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
@@ -39,8 +72,23 @@ contract StarNotary is ERC721Token {
 
     }
 
-    function clearPreviousStarState(uint256 _tokenId) private {
-        tokenToApproved[_tokenId] = address(0);
-        starForSale[_tokenId] = uint256(0);
+    function checkIfStarExist(string _ra, string _dec, string _mag) public view returns(bool) {
+        return starExists[uniqueHash(_ra,_dec,_mag)];
     }
+    function uniqueHash(string _ra,string _dec, string _mag) private pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_ra,_dec,_mag));
+    }
+
+    function tokenIdToStarInfo(uint256 _tokenId) public view returns(string,string,string,string,string) {
+        require(bytes(tokenIdToStarMapping[_tokenId].dec).length != 0,"invalid tokenId??");
+        Star memory star = tokenIdToStarMapping[_tokenId];
+        return (star.name,
+                star.starStory,
+                string(abi.encodePacked("ra_",star.ra)),
+                string(abi.encodePacked("dec_",star.dec)),
+                string(abi.encodePacked("mag_",star.mag)));
+        
+
+    }
+
 }
--- test/StarNotaryTest.js
+++ test/StarNotaryTest.js
@@ -2,30 +2,79 @@ const StarNotary = artifacts.require('StarNotary');
 
 contract('StarNotary Tests', accounts => {
   let owner = accounts[0];
+  let starId = 1;
+  let name = "My Awesome Star!";
+  let ra = "032.155";
+  let dec = "121.874";
+  let mag = "245.978";
+  let starStory = "I bought this star for my dog's birthday";
+  let tokenId = 1;
+  let user1 = accounts[1];
+  let user2 = accounts[2];
 
   beforeEach(async () => {
     this.contract = await StarNotary.new({from: owner});
   });
 
+  describe("checkIfStarExist",() => {
+    it("returns true when a star exists", async () => {
+      await this.contract.createStar(name,ra,dec,mag,starStory, tokenId, {from: owner});
+      assert.equal(await this.contract.checkIfStarExist(ra,dec,mag),true);
+    });
+    it("returns false when a star does not exist", async () => {
+      assert.equal(await this.contract.checkIfStarExist(ra,dec,mag),false);
+    });
+  });
+
   describe("can create a star",() => {
     it("can create a star and gets its name", async () => {
-      let tokenId = 1;
-      await this.contract.createStar("My Awesome Star!", tokenId, {from: owner});
-      assert.equal(await this.contract.tokenIdToStarInfo(tokenId),"My Awesome Star!");
+      await this.contract.createStar(name,ra,dec,mag,starStory, tokenId, {from: owner});
+      let starInfo = await this.contract.tokenIdToStarInfo(tokenId);
+      assert.equal(starInfo[0],name);
+      assert.equal(starInfo[1],starStory);
+      assert.equal(starInfo[2],"ra_"+ra);
+      assert.equal(starInfo[3],"dec_"+dec);
+      assert.equal(starInfo[4],"mag_"+mag);
+    });
+  });
+
+  describe('star uniqueness', () => { 
+    it('only stars unique stars can be minted', async () => { 
+      // first we mint our first star
+      await this.contract.createStar(name,ra,dec,mag,starStory, tokenId, {from: owner});
+      // then we try to mint the same star, and we expect an error
+      await expectThrow(this.contract.createStar(name,ra,dec,mag,starStory, tokenId, {from: owner}) );
     })
-  })
+
+    it('only stars unique stars can be minted even if their ID is different', async () => { 
+      // first we mint our first star
+      // then we try to mint the same star, and we expect an error
+    })
+
+    it('minting unique stars does not fail', async () => { 
+      for(let i = 1; i < 10; i ++) { 
+        let id = i
+        let newRa = i.toString()
+        let newDec = i.toString()
+        let newMag = i.toString()
+
+        await this.contract.createStar(name, starStory, newRa, newDec, newMag, id, {from: user1})
+
+        let starInfo = await this.contract.tokenIdToStarInfo(id)
+        assert.equal(starInfo[0], name)
+      }
+    })
+  });
 
   describe("buying and selling stars", () => {
-      let user1 = accounts[1];
-      let user2 = accounts[2];
-      let starId = 1;
       let starPrice = web3.toWei(.01,"ether");
 
       beforeEach(async () => {
-        await  this.contract.createStar("The awesome star!", starId, {from: user1});
+        await this.contract.createStar(name,ra,dec,mag,starStory, tokenId, {from: user1});
+        // await  this.contract.createStar("The awesome star!", starId, {from: user1});
       });
 
-      describe("user1 can sell a star", () => {
+      describe("owner can sell a star", () => {
         beforeEach(async () => {
          await this.contract.putStarUpForSale(starId, starPrice, { from: user1 });
         });
@@ -33,7 +82,7 @@ contract('StarNotary Tests', accounts => {
           assert.equal(await this.contract.starForSale(starId), starPrice);
         });
 
-        it("user1 gets paid after selling the star", async () =>  { 
+        it("owner gets paid after selling the star", async () =>  { 
           const user1BalanceBeforeTransaction = web3.eth.getBalance(user1);
           await this.contract.buyStar(starId, { from: user2, value: starPrice });
           const user1BalanceAfterTransaction = web3.eth.getBalance(user1);
@@ -62,3 +111,14 @@ contract('StarNotary Tests', accounts => {
 
   });
 });
+
+var expectThrow = async function(promise) { 
+    try { 
+        await promise
+    } catch (error) { 
+        assert.exists(error)
+        return 
+    }
+
+    assert.fail('expected an error, but none was found')
+}
